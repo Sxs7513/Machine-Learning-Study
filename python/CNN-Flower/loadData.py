91d6c367fa8335499593589616f8d94ed90fec4d
xssong <xssong@ctrip.com>
2019-01-25 15:22:01 +0800

    更新

---
+++ python/CNN-Flower/loadData.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+import os
+
+url = "http://download.tensorflow.org/example_images/flower_photos.tgz"
+
+def maybe_download(filename, work_directory):
+  """Download the data from Yann's website, unless it's already here."""
+  if not tf.gfile.Exists(work_directory):
+    tf.gfile.MakeDirs(work_directory)
+  filepath = os.path.join(work_directory, filename)
+  if not tf.gfile.Exists(filepath):
+    filepath, _ = urllib.request.urlretrieve(SOURCE_URL + filename, filepath)
+    with tf.gfile.GFile(filepath) as f:
+      size = f.size()
+    print('Successfully downloaded', filename, size, 'bytes.')
+  return filepath
--- python/git.bat
+++ python/git.bat
@@ -1,7 +1,14 @@
 @echo off
-@title bat git定时提交
+@title "git定时提交"
+echo =======================================================
+echo          Starting automatic git commit push
+echo =======================================================
+
 D:
 cd D:\Users\xssong\study\machine-learing\python
 git add .
 git commit -m "add"
 git push origin master
+
+echo done
+pause
