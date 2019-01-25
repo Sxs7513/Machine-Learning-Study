cadd5e6ef2aeb6d0008a6317bc0f8b3e2352366b
xssong <xssong@ctrip.com>
2019-01-25 17:59:05 +0800

    add

---
+++ python/CNN-Flower/index.py
@@ -0,0 +1,7 @@
+from loadData import read_data_sets
+
+data, label = read_data_sets()
+print(data)
+print(data.shape)
+print(label)
+print(label.shape)
--- python/CNN-Flower/loadData.py
+++ python/CNN-Flower/loadData.py
@@ -1,20 +1,26 @@
 import tensorflow as tf
+import numpy as np
+from skimage import io,transform
 
 import os
 import urllib
+import glob
 
-url = "http://download.tensorflow.org/example_images/flower_photos.tgz"
+SOURCE_URL = "http://download.tensorflow.org/example_images/"
 
-filename = ''
+def read_data_sets(data_dir = ''):
+    path = '.\\data\\flower_photos'
+    path = os.path.abspath(path)
 
-def maybe_download(filename, work_directory):
-  """Download the data from Yann's website, unless it's already here."""
-  if not tf.gfile.Exists(work_directory):
-    tf.gfile.MakeDirs(work_directory)
-  filepath = os.path.join(work_directory, filename)
-  if not tf.gfile.Exists(filepath):
-    filepath, _ = urllib.request.urlretrieve(SOURCE_URL + filename, filepath)
-    with tf.gfile.GFile(filepath) as f:
-      size = f.size()
-    print('Successfully downloaded', filename, size, 'bytes.')
-  return filepath
+    cate=[path + x for x in os.listdir(path) if os.path.isdir(path + '\\' + x)]
+    imgs = []
+    labels = []
+    for idx, folder in enumerate(cate):
+        for im in glob.glob(folder + '\\*.jpg'):
+            print('reading the images: %s' %s (im))
+            img = io.imread(im)
+            img = transform.resize(img, (w, h))
+            imgs.append(img)
+            labels.append(idx)
+    return np.asarray(imgs, np.float32), np.asarray(labels, np.int32)
+    
