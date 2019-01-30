47fadfcbf9e61d022e2530768ed53449851379d0
xssong <xssong@ctrip.com>
2019-01-30 19:29:50 +0800

    add

---
+++ python/R-CNN/common.py
@@ -0,0 +1,27 @@
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.normalization import local_response_normalization
+import config
+
+# 创建标准的 alexnet network
+def create_base_alexnet(input_shape = [None, config.IMAGE_SIZE, config.IMAGE_SIZE, 3]):
+    # Building 'AlexNet'
+    network = input_data(shape=input_shape)
+    network = conv_2d(network, 96, 11, strides=4, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = conv_2d(network, 256, 5, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 256, 3, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = fully_connected(network, 4096, activation='tanh')
+    network = dropout(network, 0.5)
+    network = fully_connected(network, 4096, activation='tanh')
+    network = dropout(network, 0.5)
+
+    return network
