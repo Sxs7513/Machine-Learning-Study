5873d0b84dd34e7c91ff6762d9d7bf681c348459
Sxs7513 <526015242@aa.com>
2019-06-11 13:11:42 +0800

    add

---
+++ python/fast-neural-style/utils.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import yaml
+
+slim = tf.contrib.slim
+
+
+class Flag(object):
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+
+def read_conf_file(conf_file):
+    with open(conf_file) as f:
+        FLAGS = Flag(**yaml.load(f))
+    return FLAGS
