304cc0f82030b2596745e631622ad1bf268f4fc7
Sxs7513 <526015242@aa.com>
2019-06-18 11:13:02 +0800

    add

--- python/Image-Processing/10.py
+++ python/Image-Processing/10.py
@@ -41,7 +41,7 @@ class HazeRemoval:
         self.t = 0.1
 
     
-    def boxFilter(img, r):
+    def boxFilter(self, img, r):
         (rows, cols) = img.shape
         imDst = np.zeros_like(img)
         
@@ -58,7 +58,7 @@ class HazeRemoval:
         imCum = np.cumsum(imDst, 1)
         imDst[:, 0:r+1] = imCum[:, r:2*r+1]
         imDst[:, r+1:cols-r] = imCum[:, 2*r+1:cols] - imCum[:, 0:cols-2*r-1]
-        imDst[:, cols-r:cols] = np.tile(imCum[:, cols-1], [1, r]).T - imCum[:, cols-2*r-1:cols-r-1]
+        imDst[:, cols-r: cols] = np.tile(imCum[:, cols-1], [r, 1]).T - imCum[:, cols-2*r-1 : cols-r-1]
 
         return imDst
 
@@ -67,17 +67,37 @@ class HazeRemoval:
     # I => 导向图, 常与 p 是一个东西
     # p => 带有噪声的图
     # r => 窗口大小
-    def guidedfilter(I, p, r, eps):
+    def guidedfilter(self, I, p, r, eps):
         boxFilter = self.boxFilter
         rows, cols = I.shape
         N = boxFilter(np.ones([rows, cols]), r)
 
         meanI = boxFilter(I, r) / N
+        meanP = boxFilter(p, r) / N
+        corrIp = boxFilter(I * p, r) / N
+        corrI = boxFilter(I * I, r) / N
 
+        varI = corrI - meanI * meanI
+        covIp = corrIp - meanI * meanP
+
+        a = covIp / (varI + eps)
+        b = meanP - a * meanI
+
+        meanA = boxFilter(a, r) / N
+        meanB = boxFilter(b, r) / N
+
+        q = meanA * I + meanB
+        return q
 
     
+    def _ind2sub(self, array_shape, ind):
+        rows = (ind.astype('int') % array_shape[1])
+        cols = (ind.astype('int') % array_shape[1]) # or numpy.mod(ind.astype('int'), array_shape[1])
+        return (rows, cols)
+
+
     def _rgb2gray(self, rgb):
-        return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+        return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
 
 
     def haze_removal(self):
@@ -88,7 +108,7 @@ class HazeRemoval:
 
         # # 该算法直接取最大值并取平均作为 A, 但是根据 https://cloud.tencent.com/developer/article/1011698
         # # 的看法, 这是不可取的
-        # i, j = np.argmax(darkImage)
+        # (i, j) = self._ind2sub(darkImage.shape, darkImage.argmax())
         # # 原始论文中的A最终是取原始像素中的某一个点的像素, 但是该算法中取的符合条件点的平均值作为A的值
         # A = np.mean(img[i, j, :])
 
@@ -101,18 +121,32 @@ class HazeRemoval:
         for lmax in range(bins - 1, 0, -1):
             if d[lmax] <= 0.999:
                 break
-        A = np.mean(m, axis=2)[darkImage >= ht[1][lmax]].max()
+        A = np.mean(img, axis=2)[darkImage >= ht[1][lmax]].max()
+
+
         t = 1 - self.omega * darkImage / A       
 
         # 导向滤波 https://blog.csdn.net/baimafujinji/article/details/74750283
         # https://github.com/pfchai/GuidedFilter/blob/master/guidedfilter.py
-        t = guidedfilter(grayImage, t, self.r, self.eps )
+        t = self.guidedfilter(grayImage, t, self.r, self.eps)
+        t[t < self.t] = self.t
 
+        resultImage = np.zeros_like(img)
+        for i in range(3):
+            resultImage[:, :, i] = (img[:, :, i] - A) / t + A
+
+        resultImage[resultImage < 0] = 0
+        resultImage[resultImage > 1] = 1
+
+        return resultImage
 
 
 if __name__ == '__main__':
     img = cv2.imread('./img/5.jpeg')
 
-    img = haze_removal(img / 255.0)
-    cv2.imshow('image', img)
+    # result = haze_removal(img / 255.0)
+    hz = HazeRemoval(img)
+    result = hz.haze_removal()
+
+    cv2.imshow('image', result)
     cv2.waitKey(0) 
