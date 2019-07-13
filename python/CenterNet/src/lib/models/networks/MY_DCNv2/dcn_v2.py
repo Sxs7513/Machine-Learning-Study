c4f8507a63fde4fe75bc9baedfdbda69227524ac
Sxs7513 <526015242@qq.com>
2019-07-13 22:43:56 +0800

    add

--- python/CenterNet/src/lib/models/networks/MY_DCNv2/dcn_v2.py
+++ python/CenterNet/src/lib/models/networks/MY_DCNv2/dcn_v2.py
@@ -9,7 +9,46 @@ from torch.autograd import Function
 from torch.nn.modules.utils import _pair
 from torch.autograd.function import once_differentiable
 
-# import _ext as _backend
+import _ext as _backend
+
+
+class _DCNv2(Function):
+    @staticmethod
+    def forward(ctx, input, offset, mask, weight, bias, stride, padding,
+                dilation, deformable_groups):
+        ctx.stride = _pair(stride)
+        ctx.padding = _pair(padding)
+        ctx.dilation = _pair(dilation)
+        ctx.kernel_size = _pair(weight.shape[2:4])
+        ctx.deformable_groups = deformable_groups
+        output = _backend.dcn_v2_forward(
+            input, weight, bias, offset, mask, ctx.kernel_size[0],
+            ctx.kernel_size[1], ctx.stride[0], ctx.stride[1], ctx.padding[0],
+            ctx.padding[1], ctx.dilation[0], ctx.dilation[1],
+            ctx.deformable_groups)
+        ctx.save_for_backward(input, offset, mask, weight, bias)
+        return output
+
+    @staticmethod
+    @once_differentiable
+    def backward(ctx, grad_output):
+        input, offset, mask, weight, bias = ctx.saved_tensors
+        grad_input, grad_offset, grad_mask, grad_weight, grad_bias = \
+            _backend.dcn_v2_backward(input, weight,
+                                     bias,
+                                     offset, mask,
+                                     grad_output,
+                                     ctx.kernel_size[0], ctx.kernel_size[1],
+                                     ctx.stride[0], ctx.stride[1],
+                                     ctx.padding[0], ctx.padding[1],
+                                     ctx.dilation[0], ctx.dilation[1],
+                                     ctx.deformable_groups)
+
+        return grad_input, grad_offset, grad_mask, grad_weight, grad_bias,\
+            None, None, None, None,
+
+
+dcn_v2_conv = _DCNv2.apply
 
 
 class DCNv2(nn.Module):
@@ -34,7 +73,7 @@ class DCNv2(nn.Module):
             torch.Tensor(out_channels, in_channels, *self.kernel_size))
         self.bias = nn.Parameter(torch.Tensor(out_channels))
         self.reset_parameters()
-    
+
     def reset_parameters(self):
         n = self.in_channels
         # 每个卷积核的参数数量
@@ -45,4 +84,44 @@ class DCNv2(nn.Module):
         self.weight.data.uniform_(-stdv, stdv)
         self.bias.data.zero_()
 
+    def forward(self, input, offset, mask):
+        pass
 
+
+class DCN(DCNv2):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride,
+                 padding,
+                 dilation=1,
+                 deformable_groups=1):
+        super(DCN, self).__init__(in_channels, out_channels, kernel_size,
+                                  stride, padding, dilation, deformable_groups)
+
+        # offset 和 mask 需要的特征图数量，即卷积核数量
+        # 一个 group 用一套
+        channels_ = self.deformable_groups * 3 * self.kernel_size[
+            0] * self.kernel_size[1]
+        self.conv_offset_mask = nn.Conv2d(self.in_channels,
+                                          channels_,
+                                          kernel_size=kernel_size,
+                                          stride=self.stride,
+                                          padding=self.padding,
+                                          bias=True)
+        self.init_offset()
+
+    def init_offset(self):
+        self.conv_offset_mask.weight.data.zero_()
+        self.conv_offset_mask.bias.data.zero_()
+
+    def forward(self):
+        out = self.conv_offset_mask(input)
+        o1, o2, mask = torch.chunk(out, 3, dim=1)
+        offset = torch.cat((o1, o2), dim=1)
+        mask = torch.sigmiod(mask)
+
+        return dcn_v2_conv(input, offset, mask, self.weight, self.bias,
+                           self.stride, self.padding, self.dilation,
+                           self.deformable_groups)
---
+++ python/CenterNet/src/lib/models/networks/MY_DCNv2/src/cuda/dcn_v2_cuda.cu
@@ -0,0 +1,54 @@
+#include <vector>
+#include "cuda/dcn_v2_im2col_cuda.h"
+
+#include <ATen/ATen.h>
+#include <ATen/cuda/CUDAContext.h>
+
+#include <THC/THC.h>
+#include <THC/THCAtomics.cuh>
+#include <THC/THCDeviceUtils.cuh>
+
+extern THCState *state;
+
+
+at::Tensor
+dcn_v2_cuda_forward(const at::Tensor &input, 
+                    const at::Tensor &weight,
+                    const at::Tensor &bias,
+                    const at::Tensor &offset,
+                    const at::Tensor &mask,
+                    const int kernel_h,
+                    const int kernel_w,
+                    const int stride_h,
+                    const int stride_w,
+                    const int pad_h,
+                    const int pad_w,
+                    const int dilation_h,
+                    const int dilation_w,
+                    const int deformable_group)
+{
+    using scalar_t = float;
+
+    AT_ASSERTM(input.type().is_cuda(), "input must be a CUDA tensor");
+    AT_ASSERTM(weight.type().is_cuda(), "weight must be a CUDA tensor");
+    AT_ASSERTM(bias.type().is_cuda(), "bias must be a CUDA tensor");
+    AT_ASSERTM(offset.type().is_cuda(), "offset must be a CUDA tensor");
+    AT_ASSERTM(mask.type().is_cuda(), "mask must be a CUDA tensor");
+    
+    const int batch = input.size(0);
+    const int channels = input.size(1);
+    const int height = input.size(2);
+    const int width = input.size(3);
+
+    const int channels_out = weight.size(0);
+    const int channels_kernel = weight.size(1);
+    const int kernel_h = weight.size(2);
+    const int kernel_w = weight.size(3);
+
+    AT_ASSERTM(kernel_h_ == kernel_h && kernel_w_ == kernel_w,
+        "Input shape and kernel shape wont match: (%d x %d vs %d x %d).", kernel_h_, kernel_w, kernel_h_, kernel_w_);
+
+    AT_ASSERTM(channels == channels_kernel,
+        "Input shape and kernel channels wont match: (%d vs %d).", channels, channels_kernel);
+
+}
